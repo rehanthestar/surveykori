fb112f1a336df04b3248df66af5b5747880e26e0
naziatnawmi707-code <naziatnawmi707@gmail.com>
2026-09-02 04:27:29 +0600

    take.php done

--- surveykori/app/views/survey/take.php
+++ surveykori/app/views/survey/take.php
@@ -56,58 +56,14 @@
                             <p class="error-text"></p>
                         </div>
                     <?php endforeach; ?>
-                    
-                    <div class="card take-question" data-required="1" style="display:none">
-                        <h3>1. How many hours do you study every day? <span class="text-danger">*</span></h3>
-                        <input class="input" type="text" maxlength="255">
-                        <p class="error-text"></p>
-                    </div>
-                    <div class="card take-question" data-required="1" style="display: none;">
-                        <h3>2. Where do you ususally study? <span class="text-danger">*</span></h3>
-                        <label class="check-line"><input type="radio" name="q2" value="Home">Home</label>
-                        <label class="check-line"><input type="radio" name="q2" value="Library">Library</label>
-                        <label class="check-line"><input type="radio" name="q2" value="Campus">Campus</label>
-                        <label class="check-line"><input type="radio" name="q2" value="Other">Other</label>
-                        <p class="error-text"></p>
-                    </div>
-                    <div class="card take-question" data-required="0" style="display: none;">
-                        <h3>3. Which study tools do you use?</h3>
-                        <label class="check-line"><input type="checkbox" name="q3" value="Printed books"> Printed books</label>
-                        <label class="check-line"><input type="checkbox" name="q3" value="E-books"> E-books</label>
-                        <label class="check-line"><input type="checkbox" name="q3" value="Video lectures"> Video lectures</label>
-                        <label class="check-line"><input type="checkbox" name="q3" value="Group study"> Group study</label>
-                        <p class="error-text"></p>
-                    </div>
-                    <div class="card take-question" data-required="1" style="display: none;">
-                        <h3>4. Rate Your current study routine. <span class="text-danger">*</span></h3>
-                        <div class="rating-group">
-                            <label class="check-line"><input type="radio" name="q4" value="1"> 1 &#9733;</label>
-                            <label class="check-line"><input type="radio" name="q4" value="2"> 2 &#9733;</label>
-                            <label class="check-line"><input type="radio" name="q4" value="3"> 3 &#9733;</label>
-                            <label class="check-line"><input type="radio" name="q4" value="4"> 4 &#9733;</label>
-                            <label class="check-line"><input type="radio" name="q4" value="5"> 5 &#9733;</label>
-                        </div>
-                        <p class="error-text"></p>
-                    </div>
-                    <div class="card take-question" data-required="1" style="display: none;">
-                        <h3>5. Describe one problem that distrubs your study. <span class="text-danger">*</span></h3>
-                        <textarea class="textarea" maxlength="2000"></textarea>
-                        <p class="error-text"></p>
-                    </div>
-                    
-                    <div class="row">
+
+                    <div class= "row">
                         <button type="button" class="btn btn-outline" id="prevBtn" onclick="prevQuestion()">Previous</button>
                         <button type="button" class="btn btn-primary" id="nextBtn" onclick="nextQuestion()">Next</button>
                         <button type="submit" class="btn btn-success" id="submitBtn" style="display: none;">Submit Survey</button>
                     </div>
 
                 </form>
-            </main>
-        </div>
-        <footer class="footer">
-            <p>Survey Kori &mdash; University Web Technology Project</p>
-        </footer>
-        <script src="../assets/js/script.js"></script>
-        <script src="../assets/js/survey.js"></script>
-    </body>
-</html>
+            
+        <script src="<?php echo BASE_URL; ?>assets/js/survey.js"></script>
+       
