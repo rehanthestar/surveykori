bcdd9fafa85b8715a9e0ef90a6e914127c76830e
Rehan Khan <itpagla24@gmail.com>
2026-09-01 16:16:04 +0600

    Reusable header footer and sidebar with admin sidebar hase been created for others view works

--- surveykori/index.php
+++ surveykori/index.php
@@ -1,26 +1,6 @@
 <?php
-    define('ROOT_PATH', dirname(__DIR__));
-    require ROOT_PATH . '/config/app.php';
-
-    spl_autoload_register(function(string $class): void {
-        $folders = ['Core', 'Controllers', 'Models'];
-        foreach ($folders as $folder) {
-            $file = ROOT_PATH . '/app/' . $folder . '/' . $class . '.php';
-            if (is_file($file)) {
-                require_once $file;
-                return;
-            }
-        }
-    });
+    require_once __DIR__ . '/app/core/App.php';
 
 
-    require ROOT_PATH . '/app/helpers.php';
-    $database = new Database(require ROOT_PATH . '/config/database.php');
-    Auth::init($database);
-
-    $router = new Router($database);
-    $registerRoutes = require ROOT_PATH . '/config/routes.php';
-    $registerRoutes($router);
-    $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
-
+    dispatch('IndexController', 'index.php', 'blank');
 ?>
