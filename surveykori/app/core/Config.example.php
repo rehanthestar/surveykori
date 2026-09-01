30a34040035fd4999b97619afc73517b325fa6bc
Rehan Khan <itpagla24@gmail.com>
2026-09-01 19:43:06 +0600

    solving git ignonore issue

--- surveykori/app/core/Config.example.php
+++ surveykori/app/core/Config.example.php
@@ -6,7 +6,7 @@ define('DB_NAME', 'survey_kori');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-define('BASE_URL', '/surveykori/surveykori/surveykori');
+define('BASE_URL', '/surveykori/surveykori');
 define('APP_NAME', 'Survey Kori');
 
 define('APP_PATH', dirname(__DIR__));
