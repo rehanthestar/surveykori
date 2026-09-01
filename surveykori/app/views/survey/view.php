250935d1c8cc0bf04577763142b4764ee83fbd72
naziatnawmi707-code <naziatnawmi707@gmail.com>
2026-09-02 04:02:58 +0600

    table added

--- surveykori/app/views/survey/view.php
+++ surveykori/app/views/survey/view.php
@@ -1,19 +1,19 @@
 
-        <h1>Campus Transport Experience</h1>
+        <h1><?php echo e($survey['title']); ?></h1>
         <p class="text-muted">
-            <span class="badge badge-active">Active</span>
-            <span class="badge badge-cat">Social</span>
+            <?php echo survey_badge($survey['status']); ?>
+            <span class="badge badge-cat"><?php echo e($survey['category']); ?></span>
         </p>
         <div class="card" style="max-width:700px">
-            <p>How do students travel to campus every day and what problems do they face?<br>
-               Your answers help improve the university shuttle service.</p>
+            <p><?php echo e($survey['description']); ?></p>
             <table class="table">
-                <tr><th>Creator</th><td>Ayesha Rahman</td></tr>
-                <tr><th>Questions</th><td>6</td></tr>
-                <tr><th>Reward</th><td>5 points</td></tr>
-                <tr><th>Responses</th><td>8 / 25</td></tr>
-                <tr><th>Deadline</th><td>20 Sep 2026</td></tr>
+                <tr><th>Creator</th><td><?php echo e($survey['full_name']); ?></td></tr>
+                <tr><th>Questions</th><td><?php echo $count; ?></td></tr>
+                <tr><th>Reward</th><td><?php echo (int)$survey['reward_per_response']; ?> points</td></tr>
+                <tr><th>Responses</th><td><?php echo (int)$survey['collected_responses']; ?> / <?php echo (int)$survey['required_responses']; ?></td></tr>
+                <tr><th>Deadline</th><td><?php echo nice_date($survey['deadline']); ?></td></tr>
             </table>
+            
             <a class="btn btn-primary mt" href="take.html">Start Survey</a>
             <a class="btn btn-outline mt" href="find.html">Back to list</a>
         </div>
